5384c115bda22d5c76e9df5a281253be79608fff
Shashwat Mishra <shashwatmishra290@gmail.com>
2024-09-02 16:49:01 +0530

    Addition Of Two Numbers

---
+++ file1.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the first number")
+b=int(input("Enter the second number")
+c=a+b
+print("The result of addition of two numbers is
+[A[A[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C=",c)
