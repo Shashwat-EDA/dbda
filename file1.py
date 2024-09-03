452fbf15ccafde9f915ab8b935f3f799a3b296df
Shashwat Mishra <shashwatmishra290@gmail.com>
2024-09-03 14:49:46 +0530

    Added The subtraction code

--- file1.py
+++ file1.py
@@ -3,3 +3,7 @@ b=int(input("Enter the second number")
 c=a+b
 print("The result of addition of two numbers is
 [A[A[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C=",c)
+a=int(input("Enter the number you want="))
+b=int(input("Enter the second number you want="))
+c=a-b
+print("The result is=",c)
