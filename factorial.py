2ab7f7f0aa26d436f345284ab08ccf7a40a101ae
Shashwat Mishra <shashwatmishra290@gmail.com>
2024-09-04 13:47:41 +0530

    for loop is on

---
+++ factorial.py
@@ -0,0 +1,7 @@
+num=int(input("Enter the number you want factorial of="))
+fact=1
+for(i=1;i<=num;i++)
+{
+	fact=fact*i
+}
+print("The Factorial of a number is"[D=",fact)
---
+++ file.py
@@ -0,0 +1,8 @@
+e=67
+f=52
+g=e-f
+print("The Result is :",f)[g[C
+for(i=0;i<5;i++)
+{
+	print("Count:",i)
+}
